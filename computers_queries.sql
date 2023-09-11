c748420787160462ebdf699547294d86eb6d5b9d
hypahypahypa <130292000+hypahypahypa@users.noreply.github.com>
2023-09-11 19:20:30 +0300

    Update computers_queries.sql

--- computers_queries.sql
+++ computers_queries.sql
@@ -149,6 +149,21 @@ HAVING count(uq.hd_rank) >= 2;
 -- Задание 16
 -- Найдите пары моделей PC, имеющих одинаковые скорость и RAM. В результате каждая пара указывается только один раз, т.е. (i,j), но не (j,i). 
 -- Порядок вывода: модель с большим номером, модель с меньшим номером, скорость и RAM.
+SELECT P.model, L.model, P.speed, P.ram
+FROM PC p
+JOIN 
+     (SELECT speed, ram
+      FROM PC
+      GROUP BY speed, ram
+      HAVING SUM(speed)/speed = 2 AND 
+             SUM(ram)/ram = 2 
+      ) S 
+      ON P.speed = S.speed AND 
+         P.ram = S.ram 
+JOIN PC L 
+	ON L.speed = S.speed AND 
+       L.ram = S.ram AND 
+       L.model < P.model;
 
 
 -- Задание 17
