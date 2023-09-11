bbeb09691c4406a2d947bca2f4810720083320e9
hypahypahypa <130292000+hypahypahypa@users.noreply.github.com>
2023-09-11 13:41:41 +0300

    Update computers_queries.sql

--- computers_queries.sql
+++ computers_queries.sql
@@ -154,3 +154,7 @@ HAVING count(uq.hd_rank) >= 2;
 -- Задание 17
 -- Найдите модели ПК-блокнотов, скорость которых меньше скорости каждого из ПК.
 -- Вывести: type, model, speed
+SELECT DISTINCT p.type, p.model, l.speed
+FROM product p
+JOIN laptop l ON p.model = l.model
+WHERE l.speed < ALL (SELECT pc.speed FROM pc)
