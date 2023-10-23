b40ce5d542ba728887492c9be628d90ac605c583
Egor Kukushkin <130292000+hypahypahypa@users.noreply.github.com>
2023-10-23 23:15:31 +0300

    Update computers_queries.sql

--- computers_queries.sql
+++ computers_queries.sql
@@ -640,3 +640,11 @@ select
   sum(coalesce(inc,0))-sum(coalesce(out,0)) as remain
   from income_o i
   full join outcome_o o on i.date=o.date and i.point=o.point
+
+-- Задание 62
+-- Посчитать остаток денежных средств на всех пунктах приема на начало дня 15/04/01 для базы данных с отчетностью не чаще одного раза в день.
+select
+  sum(coalesce(inc,0))-sum(coalesce(out,0)) as remain
+  from income_o i
+  full join outcome_o o on i.date=o.date and i.point=o.point
+  where coalesce(i.date,o.date) < '2001-04-15'
